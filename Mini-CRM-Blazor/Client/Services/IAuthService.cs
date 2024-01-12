d40a0a919f2327b8bf1255c9114c2bb60240bed6
matheuspolidoro <matheuspolidoro@outlook.com>
2024-01-12 21:22:01 +0100

    feat: state provider for auth and service for http login calls

---
+++ Mini-CRM-Blazor/Client/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Mini_CRM_Blazor.Shared.Models;
+
+namespace Mini_CRM_Blazor.Client.Services
+{
+    public interface IAuthService
+    {
+        Task Login(LoginRequest loginRequest);
+        Task Register(RegisterRequest registerRequest);
+        Task Logout();
+        Task<CurrentUser> CurrentUserInfo();
+    }
+}

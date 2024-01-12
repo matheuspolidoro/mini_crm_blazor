140b060a80ea43139ac68b7314677fe5195c4f38
matheuspolidoro <matheuspolidoro@outlook.com>
2024-01-12 21:16:18 +0100

    feat: generate migration for identity

---
+++ Mini-CRM-Blazor/Server/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mini_CRM_Blazor.Server.Models;
+
+namespace Mini_CRM_Blazor.Server.DAL
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+    }
+}

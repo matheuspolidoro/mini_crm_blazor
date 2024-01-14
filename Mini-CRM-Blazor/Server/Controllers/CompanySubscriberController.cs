9be9640cf25351c486b3c0f458f307c5470ce302
matheuspolidoro <matheuspolidoro@outlook.com>
2024-01-14 14:24:16 +0100

    feat: adds controller for customer and company

---
+++ Mini-CRM-Blazor/Server/Controllers/CompanySubscriberController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Mini_CRM_Blazor.Server.Services;
+using Mini_CRM_Blazor.Shared.Dtos;
+
+namespace Mini_CRM_Blazor.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CompanySubscriberController : ControllerBase
+    {
+        private readonly CompanySubscribersService _service;
+
+        public CompanySubscriberController(CompanySubscribersService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAll();
+
+            try
+            {
+                return Ok(result.Content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _service.GetById(id);
+
+            try
+            {
+                return Ok(result.Content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[HttpPut("{id}")]
+        //[Authorize(Roles = "1, 3")]
+        //public async Task<IActionResult> Put(int id, [FromBody] CompanySubscriberUpdateRequest updateModel)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return UnprocessableEntity(ModelState);
+             
+        //    var result = await _service.Update(id, updateModel);
+
+        //    try
+        //    {
+        //        return Ok(result);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ Mini-CRM-Blazor/Server/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Mini_CRM_Blazor.Server.Services;
+using Mini_CRM_Blazor.Shared.Dtos;
+
+namespace Mini_CRM_Blazor.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomersService _service;
+
+        public CustomersController(CustomersService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetAllByCompanySubscriberId(Guid id)
+        {
+            var result = await _service.GetAllByCompanySubscriberId(id);
+
+            try
+            {
+                return Ok(result.Content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _service.GetById(id);
+
+            try
+            {
+                return Ok(result.Content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[HttpPut("{id}")]
+        //[Authorize(Roles = "1, 3")]
+        //public async Task<IActionResult> Put(int id, [FromBody] CustomerUpdateRequest updateModel)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return UnprocessableEntity(ModelState);
+             
+        //    var result = await _service.Update(id, updateModel);
+
+        //    try
+        //    {
+        //        return Ok(result);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+    }
+}
